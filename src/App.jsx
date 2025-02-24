07c2d95774884b2b86b00ae1109400913fd8db56
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-02-24 10:03:48 +0900

    Result作成

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,21 @@
 import Title from "./components/Title";
 import Form from "./components/Form";
+import Result from "./components/Result";
+import {useState} from "react";
 
 const App = () => {
+  const [city, setCity] = useState("");
+  const getWeather = (e) => {
+    e.preventDefault();
+    fetch("")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <div>
       <Title />
-      <Form />
+      <Form setCity={setCity} getWeather={getWeather} />
+      <Result />
     </div>
   );
 };
--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,8 @@
-import {useState} from "react";
-
-const Form = () => {
-  const [city, setCity] = useState("");
-  const getWeather = (e) => {
-    e.preventDefault();
-    fetch("")
-      .then(res => res.json())
-      .then(data => console.log(data))
-  }
+const Form = (props) => {
   return (
     <form>
-      <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-      <button type="submit" onClick={getWeather}>Get Weather</button>
+      <input type="text" name="city" placeholder="都市名" onChange={e => props.setCity(e.target.value)} />
+      <button type="submit" onClick={props.getWeather}>Get Weather</button>
     </form>
   );
 }
