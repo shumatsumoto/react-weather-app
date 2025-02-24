932128d828335b276976cc2ba356c991e9ec18e1
Shu Matsumoto <shumatsumoto0328@gmail.com>
2025-02-24 09:52:58 +0900

    Form作成

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -2,11 +2,16 @@ import {useState} from "react";
 
 const Form = () => {
   const [city, setCity] = useState("");
+  const getWeather = (e) => {
+    e.preventDefault();
+    fetch("")
+      .then(res => res.json())
+      .then(data => console.log(data))
+  }
   return (
     <form>
       <input type="text" name="city" placeholder="都市名" onChange={e => setCity(e.target.value)} />
-      {city}
-      <button type="submit">Get Weather</button>
+      <button type="submit" onClick={getWeather}>Get Weather</button>
     </form>
   );
 }
